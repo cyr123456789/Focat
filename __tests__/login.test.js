859d0b835a6346ceafc2a5e674d73e0436a126b2
Venus Lim <limjiayivenus@gmail.com>
2022-06-21 13:20:05 +0800

    Complete basic login unit tests that check for proper components rendering

--- __tests__/login.test.js
+++ __tests__/login.test.js
@@ -1,13 +1,20 @@
 import React from 'react';
-import { render, fireEvent } from '@testing-library/react-native';
+import { create, act } from 'react-test-renderer';
 import * as eva from '@eva-design/eva';
 import { RootSiblingParent } from 'react-native-root-siblings';
 import { EvaIconsPack } from '@ui-kitten/eva-icons';
 import { ApplicationProvider, IconRegistry } from '@ui-kitten/components';
 import Login from '../screens/Login/login';
+jest.useFakeTimers();
+
+const userObject = {
+  username: 'Test User',
+  email: 'testuser@email.com',
+  password: 'Password123!',
+};
 
 const getComponent = () => {
-  return render(
+  return create(
     <RootSiblingParent>
       <IconRegistry icons={EvaIconsPack} />
       <ApplicationProvider {...eva} theme={eva.light}>
@@ -24,36 +31,40 @@ describe('Snapshot Test', () => {
   });
 });
 
-describe('Sign Up Function', () => {
-  /**
-   * valid logid n password
-   * invalid logid n password
-   * empty
-   */
-  test('returns login page due to empty fields', () => {
-    const { queryByTestId } = getComponent();
-    const loginButton = queryByTestId('loginButton');
-    fireEvent.press(loginButton);
-    expect(queryByTestId('loginButton')).toBeTruthy();
+describe('Login Function', () => {
+  test('renders login button', () => {
+    const component = getComponent();
+    const loginButton = component.root.findByProps({
+      testID: 'loginButton',
+    });
+    expect(loginButton).toBeTruthy();
   });
 
-  // TODO
-  test('returns successful login', () => {
-    const userObject = {
-      username: 'Test User',
-      email: 'testuser@email.com',
-      password: 'Password123!',
-    };
-    const { getByPlaceholderText, queryByTestId } = getComponent();
-    const emailInput = getByPlaceholderText('Email');
-    const passwordInput = getByPlaceholderText('Password');
-    const loginButton = queryByTestId('loginButton');
-    fireEvent.changeText(emailInput, userObject.email);
-    fireEvent.changeText(passwordInput, userObject.password);
-    fireEvent.press(loginButton);
+  test('renders sign up button', () => {
+    const component = getComponent();
+    const signupButton = component.root.findByProps({
+      testID: 'signupButton',
+    });
+    expect(signupButton).toBeTruthy();
+  });
 
-    expect(queryByTestId('loginButton')).toBeTruthy();
+  test('updates email input field', () => {
+    const component = getComponent();
+    const emailInput = component.root.findByProps({ testID: 'emailInput' });
+    act(() => {
+      emailInput.props.setEmail(userObject.email);
+    });
+    expect(emailInput.props.email).toBe(userObject.email);
+  });
+
+  test('updates password input field', () => {
+    const component = getComponent();
+    const passwordInput = component.root.findByProps({
+      testID: 'passwordInput',
+    });
+    act(() => {
+      passwordInput.props.setPassword(userObject.password);
+    });
+    expect(passwordInput.props.password).toBe(userObject.password);
   });
 });
-
-describe('Login Function', () => {});
