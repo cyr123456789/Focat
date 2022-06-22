ad42c17257436778a9b1a35d47138fb2f205ba64
Chia Yun Rong <chiayunrong@outlook.com>
2022-06-23 00:56:26 +0800

    Remove redundant code

--- App.js
+++ App.js
@@ -7,19 +7,14 @@ import 'react-native-gesture-handler';
 import { RootSiblingParent } from 'react-native-root-siblings';
 import { EvaIconsPack } from '@ui-kitten/eva-icons';
 
-import { Provider } from 'react-redux';
-import { store } from './store/store';
-
 const App = () => {
   return (
-    <Provider store={store}>
-      <RootSiblingParent>
-        <IconRegistry icons={EvaIconsPack} />
-        <ApplicationProvider {...eva} theme={eva.light}>
-          <StackNavigator />
-        </ApplicationProvider>
-      </RootSiblingParent>
-    </Provider>
+    <RootSiblingParent>
+      <IconRegistry icons={EvaIconsPack} />
+      <ApplicationProvider {...eva} theme={eva.light}>
+        <StackNavigator />
+      </ApplicationProvider>
+    </RootSiblingParent>
   );
 };
 
