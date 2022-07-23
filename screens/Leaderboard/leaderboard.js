1ddcb37394bddc2f592776094d0297a1ef40d640
Chia Yun Rong <chiayunrong@outlook.com>
2022-07-23 21:02:47 +0800

    Add leaderboard

--- screens/Leaderboard/leaderboard.js
+++ screens/Leaderboard/leaderboard.js
@@ -1,12 +1,115 @@
-import React from "react";
-import { StyleSheet } from "react-native";
-import { Button, Layout, Text } from "@ui-kitten/components";
+import React, { useEffect, useState } from 'react';
+import { StyleSheet } from 'react-native';
+import { Layout, Text, Button, List, ListItem } from '@ui-kitten/components';
+import { auth, firestore } from '../../firebase';
+import {
+  collection,
+  getDocs,
+  getDoc,
+  doc,
+  updateDoc,
+  arrayRemove,
+  arrayUnion,
+  query,
+  orderBy,
+  limit,
+} from 'firebase/firestore';
 
 const Leaderboard = () => {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    fetchLeaderboardData();
+  }, []);
+
+  const fetchLeaderboardData = async () => {
+    const queryRef = collection(firestore, 'users');
+    const q = query(queryRef, orderBy('cat_cash', 'desc'), limit(100));
+    const querySnapshot = await getDocs(q);
+    const userDocSnap = await getDoc(
+      doc(firestore, 'users', auth.currentUser.uid)
+    );
+    setData([]);
+    if (userDocSnap.exists()) {
+      querySnapshot.forEach((doc) => {
+        setData((oldData) => [
+          ...oldData,
+          {
+            alreadyAdded: userDocSnap.data().sent_requests.includes(doc.id),
+            alreadyFriends: userDocSnap.data().friends.includes(doc.id),
+            id: doc.id,
+            username: doc.data().username,
+            cat_cash: doc.data().cat_cash,
+          },
+        ]);
+      });
+    }
+  };
+
+  const renderItemAccessory = (item) => {
+    if (item.alreadyAdded) {
+      return (
+        <Button
+          size="tiny"
+          onPress={() => {
+            const currentUser = auth.currentUser.uid;
+            const sent = doc(firestore, 'users', currentUser);
+            const received = doc(firestore, 'users', item.id);
+            updateDoc(sent, {
+              sent_requests: arrayRemove(item.id),
+            });
+            updateDoc(received, {
+              received_requests: arrayRemove(currentUser),
+            });
+            fetchLeaderboardData();
+            console.log('unadd');
+          }}
+        >
+          Pending
+        </Button>
+      );
+    } else if (item.alreadyFriends) {
+      return (
+        <Button disabled={true} size="tiny">
+          Added
+        </Button>
+      );
+    } else {
+      return (
+        <Button
+          size="tiny"
+          onPress={() => {
+            const currentUser = auth.currentUser.uid;
+            const sent = doc(firestore, 'users', currentUser);
+            const received = doc(firestore, 'users', item.id);
+            updateDoc(sent, {
+              sent_requests: arrayUnion(item.id),
+            });
+            updateDoc(received, {
+              received_requests: arrayUnion(currentUser),
+            });
+            fetchLeaderboardData();
+            console.log('add');
+          }}
+        >
+          Add
+        </Button>
+      );
+    }
+  };
+
+  const renderItem = ({ item }) => (
+    <ListItem
+      title={`${item.username} `}
+      description={`${item.cat_cash} `}
+      accessoryRight={() => renderItemAccessory(item)}
+    />
+  );
+
   return (
     <Layout style={styles.container}>
-      <Text>Hello World!</Text>
-      <Button style={styles.button}>Click me</Button>
+      <Text style={styles.text}>Top 100 Richest Users</Text>
+      <List data={data} renderItem={renderItem} />
     </Layout>
   );
 };
@@ -15,8 +118,9 @@ export default Leaderboard;
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
+    justifyContent: 'center',
+  },
+  text: {
+    textAlign: 'center',
   },
 });
